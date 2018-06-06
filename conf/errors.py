bf63ab2909a4edf4ab50ac3edac65c6b443dbf02
Lionel Legrand <llegrand@lusis.lu>
2018-06-06 10:02:44 +0200

    remove old folder

--- conf/errors.py
+++ conf/errors.py
@@ -22,7 +22,6 @@ DOCKERFILE_ERROR = {
     #errors template
     202 :'\t - L:{ligne}\t C:{colonne} \t| Instruction "{inst}" \t| {erreur}\n',
 
-    #Parser errors messages
     211 :'Instruction: Instruction inconnue',
     212 :'Argument: Aucun argument spécifié',
     213 :'Argument: Syntaxe: argument1 argument2 …',
@@ -30,11 +29,16 @@ DOCKERFILE_ERROR = {
     215 :'Argument: Actuel:{nombre} | Autorisés : Min:{min}, Max:{max}',
     216 :'Option: {opt} | Option inconnue',
     217 :'Option: {opt} | Option Incompatible avec l\'instruction',
-   
-    #script errors messages
+
     221 :'Instruction: Non spécifiée ou incorrecte',
     222 :'Instruction: ARG ou FROM doit être en première position',
     223 :'Elements: "{fichiers}" - Elements spécifiés introuvables',
     224 :'Port: "{expose_port}" - Introuvable dans l’instruction ports du Docker-compose.yml ({container_ports})',
     225 :'Port: "{expose_port}" - Syntaxe incorrecte'
+}
+
+#SERVICES
+#Errors between Dockercompose & Dockerfile
+SERVICE_ERROR = {
+
 }
